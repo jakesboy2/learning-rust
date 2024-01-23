80de42c10543a7e8b7a4d5cd19816bd661c4f298
Jacob Waldrip <jacob.waldrip@Jacobs-MBP.attlocal.net>
2024-01-23 02:32:27 -0600

    Progress into second lesson

--- src/second.rs
+++ src/second.rs
@@ -1,50 +1,56 @@
-use std::mem;
-
-pub struct List {
-  head: Link,
+pub struct List<T> {
+  head: Link<T>,
 }
 
-enum Link {
-  Empty,
-  More(Box<Node>)
+type Link<T> = Option<Box<Node<T>>>;
+
+struct Node<T> {
+  elem: T,
+  next: Link<T>
 }
 
-struct Node {
-  elem: i32,
-  next: Link
-}
-
-impl List {
+// First generic arg is on the impl itself
+// second is just to satisfy the struct the impl is for
+impl<T> List<T> {
   pub fn new() -> Self {
-    List { head: Link::Empty }
+    List { head: None }
   }
 
-  pub fn push(&mut self, elem: i32) {
+  pub fn push(&mut self, elem: T) {
     let new_node = Node {
-      elem: elem,
-      next: mem::replace(&mut self.head, Link::Empty)
+      elem,
+      next: self.head.take(),
     };
 
-    self.head = Link::More(Box::new(new_node))
+    self.head = Some(Box::new(new_node))
   }
 
-  pub fn pop(&mut self) -> Option<i32> {
-    match mem::replace(&mut self.head, Link::Empty) {
-      Link::Empty => None,
-      Link::More(node) => {
-        self.head = node.next;
-        Some(node.elem)
-      }
-    }
+  pub fn pop(&mut self) -> Option<T> {
+    self.head.take().map(|node| {
+      self.head = node.next;
+      node.elem
+    })
+  }
+
+  pub fn peek(&mut self) -> Option<&T> {
+    self.head.as_ref().map(|node| {
+      &node.elem
+    })
+  }
+
+  pub fn peek_mut(&mut self) -> Option<&mut T> {
+    self.head.as_mut().map(|node| {
+      &mut node.elem
+    })
   }
 }
 
-impl Drop for List {
+impl<T> Drop for List<T> {
   fn drop(&mut self) {
-    let mut current_link = mem::replace(&mut self.head, Link::Empty);
+    let mut current_link = self.head.take();
 
-    while let Link::More(mut boxed_node) = current_link {
-      current_link = mem::replace(&mut boxed_node.next, Link::Empty);
+    while let Some(mut boxed_node) = current_link {
+      current_link = boxed_node.next.take();
     }
   }
 }
@@ -75,4 +81,21 @@ mod test {
     assert_eq!(list.pop(), Some(1));
     assert_eq!(list.pop(), None);
   }
+
+  #[test]
+  fn peek() {
+    let mut list = List::new();
+    assert_eq!(list.peek(), None);
+    assert_eq!(list.peek_mut(), None);
+    list.push(1); list.push(2); list.push(3);
+
+    assert_eq!(list.peek(), Some(&3));
+    assert_eq!(list.peek_mut(), Some(&mut 3));
+    list.peek_mut().map(|value| {
+        *value = 42
+    });
+
+    assert_eq!(list.peek(), Some(&42));
+    assert_eq!(list.pop(), Some(42));
+}
 }
